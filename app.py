80ef725a1798b93ced05215e480177a2208361d1
louisgaignard1-glitch <louisgaignard1@gmail.com>
2026-02-27 23:15:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ allocation = {
 
 tickers = list(allocation.keys())
 start = st.sidebar.date_input("Start date", datetime(2020, 1, 1))
-if failed_tickers:
-    st.warning(f"Les tickers suivants n'ont pas pu être téléchargés : {', '.join(failed_tickers)}. Vérifiez leur validité.")
+
 
 # =====================
 # Chargement prix
@@ -65,7 +64,7 @@ def load_prices(tickers, start):
             failed_tickers.append(t)
 
     if failed_tickers:
-        st.warning(f"Impossible de récupérer les prix pour : {', '.join(failed_tickers)}")
+    st.warning(f"Les tickers suivants n'ont pas pu être téléchargés : {', '.join(failed_tickers)}. Vérifiez leur validité.")
 
     return prices
 
